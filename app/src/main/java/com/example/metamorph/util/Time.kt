97376f94e3bd339df80b7990eb2e7e3b66709797
Ethan Lee <33e.lee@gmail.com>
2022-08-10 21:25:57 -0700

    Fix time formatting pattern causing DateTimeParseException

--- app/src/main/java/com/example/metamorph/util/Time.kt
+++ app/src/main/java/com/example/metamorph/util/Time.kt
@@ -1,14 +1,44 @@
 package com.example.metamorph.util
 
+import android.util.Log
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeParseException
 
+/**
+ * @param date pattern can be yyyy-MM-dd'T'HH:mm:ss.SSS'Z or yyyy-MM-dd'T'HH:mm:ss.S'Z
+ * @return ex. Aug 11, 2022
+ */
 fun formatDateTime(date: String): String {
-    var formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
-    val formattedDate = LocalDate.parse(date, formatter)
+    try {
+        var formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+        val formattedDate = LocalDate.parse(date, formatter)
 
-    val month = formattedDate.format(DateTimeFormatter.ofPattern("MMM"))
-    val day = formattedDate.format(DateTimeFormatter.ofPattern("dd"))
-    val year = formattedDate.format(DateTimeFormatter.ofPattern("yyyy"))
-    return "$month $day, $year"
+        val month = formattedDate.format(DateTimeFormatter.ofPattern("MMM"))
+        val day = formattedDate.format(DateTimeFormatter.ofPattern("dd"))
+        val year = formattedDate.format(DateTimeFormatter.ofPattern("yyyy"))
+        return "$month $day, $year"
+    } catch (e: DateTimeParseException) {
+        formatDateTimeBackup(date)
+    }
+    return date
+}
+
+/**
+ * @param date pattern must be yyyy-MM-dd'T'HH:mm:ss.S'Z or DateTimeParseException is thrown
+ * @throws DateTimeParseException
+ */
+fun formatDateTimeBackup(date: String): String {
+    try {
+        var formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.S'Z'")
+        val formattedDate = LocalDate.parse(date, formatter)
+
+        val month = formattedDate.format(DateTimeFormatter.ofPattern("MMM"))
+        val day = formattedDate.format(DateTimeFormatter.ofPattern("dd"))
+        val year = formattedDate.format(DateTimeFormatter.ofPattern("yyyy"))
+        return "$month $day, $year"
+    } catch (e: DateTimeParseException) {
+        e.message?.let { Log.e("Format exception", it) }
+    }
+    return date
 }
